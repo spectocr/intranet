2a6367baec3c54ed08fecaf3536bc459146914b8
spectocr <highstakes22@gmail.com>
2021-06-26 13:22:33 -0400

    it works :/

--- app.js
+++ app.js
@@ -1,18 +1,159 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
 const path = require("path");
 const fs = require("fs");
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
+const OUTPUT_DIR = path.resolve(__dirname, "dist");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
+let employees = [];
+
+const managerEntry = (name, id, email) => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: 'Enter managers office Number',
+    },
+  ])
+    .then((managerEntry) => {
+      //console.log(name,id,email);
+      let manager = new Manager(name, id, email, managerEntry.officeNumber)
+      employees.push(manager);
+      //console.log(JSON.stringify(managerEntry, null, ''))
+      //console.log(employees);
+      return inquirer.prompt([
+        {
+          type: 'confirm',
+          name: 'addEmpolyee',
+          message: 'Add another employee?',
+        },
+      ])
+        .then((addEmpolyee) => {
+          //console.log(JSON.stringify(addEmpolyee, null, ''))
+          if (addEmpolyee.addEmpolyee == true) {
+            employeeEntry();
+            //console.log(employees);
+          } else {
+            writeToFile(employees);
+          }
+        })
+    })
+};
+
+const internEntry = (name, id, email) => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'school',
+      message: 'Enter interns school name',
+    },
+  ])
+    .then((internEntry) => {
+      let intern = new Intern(name, id, email, internEntry.school)
+      employees.push(intern);
+      //console.log(employees);
+      return inquirer.prompt([
+        {
+          type: 'confirm',
+          name: 'addEmpolyee',
+          message: 'Add another employee?',
+        },
+      ])
+        .then((addEmpolyee) => {
+          //console.log(JSON.stringify(addEmpolyee, null, ''))
+          if (addEmpolyee.addEmpolyee == true) {
+            employeeEntry();
+            //console.log(employees);
+          } else {
+            writeToFile(employees);
+          }
+        })
+    })
+};
+
+const engineerEntry = (name, id, role) => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter engineers github',
+    },
+  ])
+    .then((engineerEntry) => {
+      //console.log(JSON.stringify(engineerEntry, null, ''))
+      let engineer = new Engineer(name, id, email, engineerEntry.github)
+      employees.push(engineer);
+      //console.log(employees);
+      return inquirer.prompt([
+        {
+          type: 'confirm',
+          name: 'addEmpolyee',
+          message: 'Add another employee?',
+        },
+      ])
+        .then((addEmpolyee) => {
+          //console.log(JSON.stringify(addEmpolyee, null, ''))
+          if (addEmpolyee.addEmpolyee == true) {
+            employeeEntry();
+            //console.log(employees);
+          } else {
+            renderIt(employees);
+          }
+        })
+    })
+};
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const employeeEntry = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Enter employee name',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What employee contact email? (Required)',
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'Enter ID (Required)',
+    },
+    {
+      type: 'checkbox',
+      name: 'role',
+      message: "What is employee's role (Required)",
+      choices: ['Manager', 'Engineer', 'Intern'],
+    },
+
+  ])
+    .then((employeeEntry) => {
+      //console.log(JSON.stringify(employeeEntry, null, ''))
+      const currentEmp = employeeEntry;
+      //console.log(currentEmp);
+      switch (currentEmp.role[0]) {
+        case 'Manager':
+          managerEntry(currentEmp.name, currentEmp.id, currentEmp.email);
+          break;
+        case 'Engineer':
+          engineerEntry(currentEmp.name, currentEmp.id, currentEmp.email);
+          break;
+        case 'Intern':
+          internEntry(currentEmp.name, currentEmp.id, currentEmp.email);
+          break;
+      }
+    })
+};
+
+employeeEntry();
+
+function writeToFile(html) {
+  fs.writeFileSync(outputPath, render(html), 'utf-8');
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -32,4 +173,4 @@ const render = require("./lib/htmlRenderer");
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// for the provided `render` function to work!
